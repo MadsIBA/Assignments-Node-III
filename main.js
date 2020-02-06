0038bf5cd207df262b42681b0dca7923ac7e928b
Mads <Mads2097@gmail.com>
2020-02-06 12:17:33 +0100

    Prep for Assignment III-1 part 2
    Checkpoint from when class was over

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var server = require("./server");                   // make server module available
+var router = require("./router");                   // router module
+
+server.start(router);                               // start server
+                                                    // callback to route
